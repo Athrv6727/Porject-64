4ad9af945508e1a4c637fd0216844fbbea5ca282
Athrv6727 <73478825+Athrv6727@users.noreply.github.com>
2021-03-24 20:57:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import db from './localdb';
+import PhonicSoundButton from './components/PhonicSoundButton';
+
+export default class App extends React.Component {
+  
+  render() {
+    return (
+
+    );
+  }
+}
+
